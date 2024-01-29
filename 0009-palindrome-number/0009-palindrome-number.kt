851f7532a9e254bfc522bfe81d6132dfa53a4b8f
Akhsan <79033189+fnakhsan@users.noreply.github.com>
2024-01-29 22:25:48 +0700

    Time: 207 ms (80.70%), Space: 36.8 MB (60.28%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.kt
+++ 0009-palindrome-number/0009-palindrome-number.kt
@@ -1,5 +1,9 @@
 class Solution {
     fun isPalindrome(x: Int): Boolean {
-        return x.toString() == x.toString().reversed()
+        val str = x.toString()
+        for (i in 0 until str.length/2) {
+            if (str[i] != str[str.length - i - 1]) return false
+        }
+        return true
     }
 }
